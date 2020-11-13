007134daa813c99aa4fafdd3d9333f93dd620368
liaoyj <liaoyj@huak.com.cn>
2020-11-13 23:08:25 +0800

    健康管理项目-web: 1、引入layui框架; 2、使用layuiAdmin后台管理模板修改。

---
+++ health-web/admin/json/menu.js
@@ -0,0 +1,27 @@
+
+{
+    "code": 0,
+    "msg": "",
+    "data": [
+    {
+        "title": "主页",
+        "icon": "layui-icon-home",
+        "list": [
+            {
+                "title": "控制台",
+                "jump": "/"
+            },
+            {
+                "name": "homepage2",
+                "title": "健康报告",
+                "jump": "home/homepage2"
+            },
+            {
+                "name": "health",
+                "title": "运动数据",
+                "jump": "health/tablelist"
+            }
+        ]
+    }
+]
+}
