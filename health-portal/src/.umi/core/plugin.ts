301b5f9b3a74cea2bd42d612e5d37d4698780c1a
liaoyj <892471693@qq.com>
2021-01-05 16:59:19 +0800

    运动健康管理：   1、去掉ant-design-pro前端框架；   2、去掉系统管理模块。

--- health-portal/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'F:/ProjectSpace/GitHub/personal-health/health-portal/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','locale','locale','request',],
-});
-
-export { plugin };
--- health-portal/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'F:/ProjectSpace/GitHub/personal-health/health-portal/node_modules/react-helmet';
