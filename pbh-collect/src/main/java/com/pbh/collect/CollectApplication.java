2cd72d151e58018f67d0455f21c2727f28b74be2
liaoyj <892471693@qq.com>
2021-01-26 00:02:47 +0800

    1、integrate nacos

--- pbh-collect/src/main/java/com/pbh/collect/CollectApplication.java
+++ pbh-collect/src/main/java/com/pbh/collect/CollectApplication.java
@@ -1,6 +1,6 @@
 package com.pbh.collect;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author liaoyj
  * @date 2020-10-21
  **/
-@EnableDubbo
+//@EnableDubbo
 @MapperScan(basePackages = "com.pbh.collect.mapper")
 @SpringBootApplication
+@NacosPropertySource(dataId = "collect-application", autoRefreshed = true)
 public class CollectApplication {
 
     public static void main(String[] args) {
---
+++ pbh-collect/src/main/java/com/pbh/collect/config/NacosConfig.java
@@ -0,0 +1,33 @@
+package com.pbh.collect.config;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+
+/**
+ * Package com.pbh.collect.config
+ * ClassName NacosConfig.java
+ * Description Nacos配置类
+ *
+ * @author Liaoyj
+ * @version V1.0
+ * @date 2021-01-25 下午11:27
+ **/
+@Configuration
+public class NacosConfig {
+
+    @Value("${server.port}")
+    private int serverPort;
+    @Value("${dubbo.application.name}")
+    private String applicationName;
+    @NacosInjected
+    private NamingService namingService;
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
+    }
+
+}
